6c4c9bf0030d65005d4d0dcaa43a1412d4624750
isaias <isaiasherreroflorensa@gmail.com>
2025-02-09 18:46:19 +0100

    Commit 9

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
@@ -24,4 +21,6 @@ const nextConfig = {
   redirects,
 }
 
-export default withPayload(nextConfig)
+const configWithPayload = withPayload(nextConfig)
+
+export default configWithPayload
